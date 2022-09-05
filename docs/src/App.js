14a0e4efef677802ac511c05f20335a59a36a809
Pierre-Louis Guhur <pierre-louis.guhur@laposte.net>
2022-09-05 17:28:49 +0200

    fix: rm demo and paper

--- docs/src/App.js
+++ docs/src/App.js
@@ -1,8 +1,8 @@
 import React, {Component} from 'react';
 import Header from './components/Header';
 import Project from './components/Project';
-import Demo from './components/Demo';
-import Paper from './components/Paper';
+// import Demo from './components/Demo';
+// import Paper from './components/Paper';
 
 class App extends Component {
   render() {
