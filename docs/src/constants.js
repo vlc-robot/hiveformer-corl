4e90dfb2272ae81c065aefa2631251f1e5fd96c3
Pierre-Louis Guhur <pierre-louis.guhur@laposte.net>
2022-09-08 18:14:06 +0200

    feat: youtube link

--- docs/src/constants.js
+++ docs/src/constants.js
@@ -4,17 +4,17 @@ export const socialLinks = {
     icon: 'fa-github',
     disable: false,
   },
-  video: {
-    url: 'https://github.com/guhur/hiveformer',
+  "YouTube": {
+    url: 'https://youtu.be/nnpvOrq0XjU',
     icon: 'fa-youtube',
-    disable: true,
+    disable: false,
   },
   paper: {
     url: 'https://github.com/guhur/hiveformer',
     icon: 'fa-arxiv',
     disable: true,
   },
-  contact: {
+  "Contact": {
     url: 'mailto:pierrelouisguhur@gmail.com',
     icon: 'fa-envelope',
     disable: false,
