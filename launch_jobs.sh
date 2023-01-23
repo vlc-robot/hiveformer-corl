c1b9f643141714913cb627d27756c9dfdf781f78
Theophile Gervet <theophile.gervet@gmail.com>
2023-01-22 21:34:56 -0500

    Launch training jobs

--- launch_jobs.sh
+++ launch_jobs.sh
@@ -5,18 +5,12 @@ train_seed=0
 val_seed=1
 task_file=10_autolambda_tasks.csv
 output_dir=$root/datasets/hiveformer/packaged
-experiment=exp1
+experiment=exp2
 
-sbatch train.sh \
-  --name $experiment \
-  --tasks pick_and_lift \
-  --dataset $output_dir/$train_seed \
-  --valset $output_dir/$val_seed
-
-#for task in $(cat $task_file | tr '\n' ' '); do
-#  sbatch train.sh \
-#    --name $experiment \
-#    --tasks $task \
-#    --dataset $output_dir/$train_seed \
-#    --valset $output_dir/$val_seed
-#done
+for task in $(cat $task_file | tr '\n' ' '); do
+  sbatch train.sh \
+    --name $experiment \
+    --tasks $task \
+    --dataset $output_dir/$train_seed \
+    --valset $output_dir/$val_seed
+done
