a291ddd0e05259e5e9d83909bb716897d0cb5a27
Theophile Gervet <theophile.gervet@gmail.com>
2023-01-29 14:34:37 -0500

    Uncleaned code

--- vln-robot/alhistory/launch_jobs.sh
+++ vln-robot/alhistory/launch_jobs.sh
@@ -8,9 +8,10 @@ output_dir=$root/datasets/hiveformer/packaged
 
 experiment=uncleaned_code_exp1
 for task in $(cat $task_file | tr '\n' ' '); do
-  sbatch train_1gpu.sh \
-    --name $experiment \
-    --tasks $task \
-    --dataset $output_dir/$train_seed \
-    --valset $output_dir/$val_seed
+  echo $task
+#  sbatch train_1gpu.sh \
+#    --name $experiment \
+#    --tasks $task \
+#    --dataset $output_dir/$train_seed \
+#    --valset $output_dir/$val_seed
 done
