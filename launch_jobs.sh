14fcd3b424e72bb293acf23cc9f113772520a3fa
Theophile Gervet <theophile.gervet@gmail.com>
2023-01-30 11:17:15 -0500

    Match uncleaned code results

--- launch_jobs.sh
+++ launch_jobs.sh
@@ -6,22 +6,22 @@ val_seed=1
 task_file=10_autolambda_tasks.csv
 output_dir=$root/datasets/hiveformer/packaged
 
-#experiment=exp5
-#for task in $(cat $task_file | tr '\n' ' '); do
-#  sbatch train_1gpu.sh \
-#    --name $experiment \
-#    --tasks $task \
-#    --dataset $output_dir/$train_seed \
-#    --valset $output_dir/$val_seed
-#done
-
-experiment=ghost_points_exp1
+experiment=reproduce_uncleaned_with_released_exp1
 for task in $(cat $task_file | tr '\n' ' '); do
-  sbatch train_1gpu_32gb.sh \
+  sbatch train_1gpu.sh \
     --name $experiment \
     --tasks $task \
     --dataset $output_dir/$train_seed \
-    --valset $output_dir/$val_seed \
-    --model develop \
-    --batch_size 10
+    --valset $output_dir/$val_seed
 done
+
+#experiment=ghost_points_exp1
+#for task in $(cat $task_file | tr '\n' ' '); do
+#  sbatch train_1gpu_32gb.sh \
+#    --name $experiment \
+#    --tasks $task \
+#    --dataset $output_dir/$train_seed \
+#    --valset $output_dir/$val_seed \
+#    --model develop \
+#    --batch_size 10
+#done
