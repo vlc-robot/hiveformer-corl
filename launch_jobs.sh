decc4def35289d7b931bc3466ec49a044f7ff6de
Theophile Gervet <theophile.gervet@gmail.com>
2023-02-02 17:24:23 -0500

    Train with cross-entropy

--- launch_jobs.sh
+++ launch_jobs.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
-#task_file=10_autolambda_tasks.csv
-#for task in $(cat $task_file | tr '\n' ' '); do
-#  sbatch train_1gpu_32gb.sh \
-#     --tasks $task \
-#     --dataset /home/tgervet/datasets/hiveformer/packaged/0 \
-#     --valset /home/tgervet/datasets/hiveformer/packaged/1 \
-#     --batch_size 10 \
-#     --exp_log_dir train_cross_entropy_loss \
-#     --run_log_dir $task \
-#     --compute_loss_at_all_layers 0
-#done
+task_file=debugging_tasks.csv
+for task in $(cat $task_file | tr '\n' ' '); do
+  sbatch train_1gpu_32gb.sh \
+     --tasks $task \
+     --dataset /home/tgervet/datasets/hiveformer/packaged/0 \
+     --valset /home/tgervet/datasets/hiveformer/packaged/1 \
+     --batch_size 10 \
+     --exp_log_dir xentropy \
+     --run_log_dir $task \
+     --compute_loss_at_all_layers 0
+done
 
 task_file=debugging_tasks.csv
 for task in $(cat $task_file | tr '\n' ' '); do
@@ -19,7 +19,7 @@ for task in $(cat $task_file | tr '\n' ' '); do
      --dataset /home/tgervet/datasets/hiveformer/packaged/0 \
      --valset /home/tgervet/datasets/hiveformer/packaged/1 \
      --batch_size 10 \
-     --exp_log_dir train_cross_entropy_loss_with_intermediate_losses \
+     --exp_log_dir xentropy_intermediate_losses \
      --run_log_dir $task \
      --compute_loss_at_all_layers 1
 done
