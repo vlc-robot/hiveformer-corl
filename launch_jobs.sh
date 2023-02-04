a3fd0f818df5158d0988ce5080cce9432ad34eb6
Theophile Gervet <theophile.gervet@gmail.com>
2023-02-04 12:02:18 -0500

    More exps

--- launch_jobs.sh
+++ launch_jobs.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-main_dir=02_04
+main_dir=02_04_more
 
 # Cross-entropy
 
@@ -34,13 +34,27 @@ for task in $(cat $task_file | tr '\n' ' '); do
      --tasks $task \
      --dataset /home/tgervet/datasets/hiveformer/packaged/0 \
      --valset /home/tgervet/datasets/hiveformer/packaged/1 \
-     --exp_log_dir $main_dir/binary_xentropy_non_supervised_ball_001 \
+     --exp_log_dir $main_dir/bce_with_gt \
      --run_log_dir $task \
      --non_supervised_ball_radius 0.01 \
-     --position_loss bce
+     --position_loss bce \
+     --use_ground_truth_position_for_sampling 1
 done
 
-# MSE
+task_file=debugging_tasks.csv
+for task in $(cat $task_file | tr '\n' ' '); do
+  sbatch train_1gpu_32gb.sh \
+     --tasks $task \
+     --dataset /home/tgervet/datasets/hiveformer/packaged/0 \
+     --valset /home/tgervet/datasets/hiveformer/packaged/1 \
+     --exp_log_dir $main_dir/bce_no_gt \
+     --run_log_dir $task \
+     --non_supervised_ball_radius 0.01 \
+     --position_loss bce \
+     --use_ground_truth_position_for_sampling 0
+done
+
+# CE
 
 task_file=more_tasks.csv
 for task in $(cat $task_file | tr '\n' ' '); do
@@ -48,7 +62,22 @@ for task in $(cat $task_file | tr '\n' ' '); do
      --tasks $task \
      --dataset /home/tgervet/datasets/hiveformer/packaged/0 \
      --valset /home/tgervet/datasets/hiveformer/packaged/1 \
-     --exp_log_dir $main_dir/mse \
+     --exp_log_dir $main_dir/ce_with_gt \
      --run_log_dir $task \
-     --position_loss mse
+     --non_supervised_ball_radius 0.01 \
+     --position_loss ce \
+     --use_ground_truth_position_for_sampling 1
+done
+
+task_file=more_tasks.csv
+for task in $(cat $task_file | tr '\n' ' '); do
+  sbatch train_1gpu_32gb.sh \
+     --tasks $task \
+     --dataset /home/tgervet/datasets/hiveformer/packaged/0 \
+     --valset /home/tgervet/datasets/hiveformer/packaged/1 \
+     --exp_log_dir $main_dir/ce_no_gt \
+     --run_log_dir $task \
+     --non_supervised_ball_radius 0.01 \
+     --position_loss ce \
+     --use_ground_truth_position_for_sampling 0
 done
