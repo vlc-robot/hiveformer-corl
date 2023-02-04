af30997539ef9be57c8a9c464c5383038367c75f
Theophile Gervet <theophile.gervet@gmail.com>
2023-02-04 13:11:37 -0500

    Try without ghost points

--- launch_jobs.sh
+++ launch_jobs.sh
@@ -1,83 +1,15 @@
 #!/bin/sh
 
-main_dir=02_04_more
+main_dir=02_04_no_ghost_points
 
-# Cross-entropy
-
-#task_file=debugging_tasks.csv
-#for task in $(cat $task_file | tr '\n' ' '); do
-#  sbatch train_1gpu_32gb.sh \
-#     --tasks $task \
-#     --dataset /home/tgervet/datasets/hiveformer/packaged/0 \
-#     --valset /home/tgervet/datasets/hiveformer/packaged/1 \
-#     --exp_log_dir $main_dir/xentropy_non_supervised_ball_001 \
-#     --run_log_dir $task \
-#     --non_supervised_ball_radius 0.01
-#done
-#
-#task_file=debugging_tasks.csv
-#for task in $(cat $task_file | tr '\n' ' '); do
-#  sbatch train_1gpu_32gb.sh \
-#     --tasks $task \
-#     --dataset /home/tgervet/datasets/hiveformer/packaged/0 \
-#     --valset /home/tgervet/datasets/hiveformer/packaged/1 \
-#     --exp_log_dir $main_dir/xentropy_non_supervised_ball_003 \
-#     --run_log_dir $task \
-#     --non_supervised_ball_radius 0.03
-#done
-
-# BCE
-
-task_file=debugging_tasks.csv
+task_file=10_autolambda_tasks.csv
 for task in $(cat $task_file | tr '\n' ' '); do
-  sbatch train_1gpu_32gb.sh \
+  sbatch train_1gpu.sh \
      --tasks $task \
      --dataset /home/tgervet/datasets/hiveformer/packaged/0 \
      --valset /home/tgervet/datasets/hiveformer/packaged/1 \
-     --exp_log_dir $main_dir/bce_with_gt \
+     --exp_log_dir $main_dir \
      --run_log_dir $task \
-     --non_supervised_ball_radius 0.01 \
-     --position_loss bce \
-     --use_ground_truth_position_for_sampling 1
-done
-
-task_file=debugging_tasks.csv
-for task in $(cat $task_file | tr '\n' ' '); do
-  sbatch train_1gpu_32gb.sh \
-     --tasks $task \
-     --dataset /home/tgervet/datasets/hiveformer/packaged/0 \
-     --valset /home/tgervet/datasets/hiveformer/packaged/1 \
-     --exp_log_dir $main_dir/bce_no_gt \
-     --run_log_dir $task \
-     --non_supervised_ball_radius 0.01 \
-     --position_loss bce \
-     --use_ground_truth_position_for_sampling 0
-done
-
-# CE
-
-task_file=debugging_tasks.csv
-for task in $(cat $task_file | tr '\n' ' '); do
-  sbatch train_1gpu_32gb.sh \
-     --tasks $task \
-     --dataset /home/tgervet/datasets/hiveformer/packaged/0 \
-     --valset /home/tgervet/datasets/hiveformer/packaged/1 \
-     --exp_log_dir $main_dir/ce_with_gt \
-     --run_log_dir $task \
-     --non_supervised_ball_radius 0.01 \
-     --position_loss ce \
-     --use_ground_truth_position_for_sampling 1
-done
-
-task_file=debugging_tasks.csv
-for task in $(cat $task_file | tr '\n' ' '); do
-  sbatch train_1gpu_32gb.sh \
-     --tasks $task \
-     --dataset /home/tgervet/datasets/hiveformer/packaged/0 \
-     --valset /home/tgervet/datasets/hiveformer/packaged/1 \
-     --exp_log_dir $main_dir/ce_no_gt \
-     --run_log_dir $task \
-     --non_supervised_ball_radius 0.01 \
-     --position_loss ce \
-     --use_ground_truth_position_for_sampling 0
+     --position_loss mse \
+     --sample_ghost_points 0
 done
