af90afe3154e7ed886db60f296c5e71224c540a2
Theophile Gervet <theophile.gervet@gmail.com>
2023-02-08 18:06:30 -0500

    Debug ground-truth ball

--- launch_jobs.sh
+++ launch_jobs.sh
@@ -3,7 +3,8 @@
 main_dir=02_08_debugged
 
 task_file=debugging_tasks.csv
-for task in $(cat $task_file | tr '\n' ' '); do
+#for task in $(cat $task_file | tr '\n' ' '); do
+for task in put_money_in_safe; do
   for rad in 0 0.01; do
     for sample in 0 1; do
       sbatch train_1gpu_32gb.sh \
