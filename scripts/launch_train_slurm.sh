adfbc1e46b53d7a59d94585a214eb1bdc3ed22be
Theophile Gervet <theophile.gervet@gmail.com>
2023-02-10 19:47:11 -0500

    Cleanup

--- scripts/launch_train_slurm.sh
+++ scripts/launch_train_slurm.sh
@@ -2,20 +2,20 @@
 
 main_dir=reproduce_hiveformer
 
-#task_file=tasks/10_autolambda_tasks.csv
-#for task in $(cat $task_file | tr '\n' ' '); do
-#  sbatch train_1gpu_12gb.sh \
-#     --tasks $task \
-#     --dataset /home/tgervet/datasets/hiveformer/packaged/0 \
-#     --valset /home/tgervet/datasets/hiveformer/packaged/1 \
-#     --exp_log_dir $main_dir \
-#     --run_log_dir HIVEFORMER-$task \
-#     --model original \
-#     --batch_size 32 \
-#     --train_iters 100_000
-#done
+task_file=tasks/10_autolambda_tasks.csv
+for task in $(cat $task_file | tr '\n' ' '); do
+  sbatch train_1gpu_12gb.sh \
+     --tasks $task \
+     --dataset /home/tgervet/datasets/hiveformer/packaged/0 \
+     --valset /home/tgervet/datasets/hiveformer/packaged/1 \
+     --exp_log_dir $main_dir \
+     --run_log_dir HIVEFORMER-$task \
+     --model original \
+     --batch_size 32 \
+     --train_iters 100_000
+done
 
-task_file=tasks/debugging_tasks.csv
+task_file=tasks/2_debugging_tasks.csv
 for task in $(cat $task_file | tr '\n' ' '); do
   sbatch train_1gpu_32gb.sh \
      --tasks $task \
