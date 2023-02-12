dcffb296a11315035ee470ce65cd285d143af944
Theophile Gervet <theophile.gervet@gmail.com>
2023-02-12 12:14:13 -0500

    overfit c2f

--- scripts/launch_train_slurm.sh
+++ scripts/launch_train_slurm.sh
@@ -54,9 +54,29 @@
 
 main_dir=02_12_overfit_coarse_to_fine2
 
+#task_file=tasks/2_debugging_tasks.csv
+#dataset=/home/tgervet/datasets/hiveformer/packaged/1
+#image_size="128,128"
+#for task in $(cat $task_file | tr '\n' ' '); do
+#  for ground_truth_gaussian_spread in 0.01 0.001; do
+#    for use_ground_truth_position_for_sampling in 1; do
+#      sbatch train_1gpu_32gb.sh \
+#         --tasks $task \
+#         --dataset $dataset \
+#         --image_size $image_size \
+#         --exp_log_dir $main_dir \
+#         --run_log_dir $task-gtspread-$ground_truth_gaussian_spread-gtsample-$use_ground_truth_position_for_sampling-IMPERFECT \
+#         --coarse_to_fine_sampling 1 \
+#         --batch_size 25 \
+#         --ground_truth_gaussian_spread $ground_truth_gaussian_spread \
+#         --use_ground_truth_position_for_sampling $use_ground_truth_position_for_sampling
+#    done
+#  done
+#done
+
 task_file=tasks/2_debugging_tasks.csv
-dataset=/home/tgervet/datasets/hiveformer/packaged/1
-image_size="128,128"
+dataset=/home/tgervet/datasets/hiveformer/packaged/3
+image_size="256,256"
 for task in $(cat $task_file | tr '\n' ' '); do
   for ground_truth_gaussian_spread in 0.01 0.001; do
     for use_ground_truth_position_for_sampling in 1; do
