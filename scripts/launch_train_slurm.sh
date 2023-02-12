d6e9f34466ab4757b16a69095a24183035a394fd
Theophile Gervet <theophile.gervet@gmail.com>
2023-02-12 12:07:28 -0500

    Fix small bug

--- scripts/launch_train_slurm.sh
+++ scripts/launch_train_slurm.sh
@@ -59,13 +59,13 @@ dataset=/home/tgervet/datasets/hiveformer/packaged/1
 image_size="128,128"
 for task in $(cat $task_file | tr '\n' ' '); do
   for ground_truth_gaussian_spread in 0.01 0.001; do
-    for use_ground_truth_position_for_sampling in 0 1; do
+    for use_ground_truth_position_for_sampling in 1; do
       sbatch train_1gpu_32gb.sh \
          --tasks $task \
          --dataset $dataset \
          --image_size $image_size \
          --exp_log_dir $main_dir \
-         --run_log_dir $task-gtspread-$ground_truth_gaussian_spread-gtsample-$use_ground_truth_position_for_sampling \
+         --run_log_dir $task-gtspread-$ground_truth_gaussian_spread-gtsample-$use_ground_truth_position_for_sampling-IMPERFECT \
          --coarse_to_fine_sampling 1 \
          --batch_size 25 \
          --ground_truth_gaussian_spread $ground_truth_gaussian_spread \
