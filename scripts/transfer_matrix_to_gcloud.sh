969697d2539c4e0f9ff9300fc4abe2490ad035b4
Theophile Gervet <theophile.gervet@gmail.com>
2023-02-17 06:30:16 -0500

    transfer

--- scripts/transfer_matrix_to_gcloud.sh
+++ scripts/transfer_matrix_to_gcloud.sh
@@ -1,14 +1,34 @@
+# Ours
+
 source_prefix=home/tgervet/hiveformer/train_logs
 target_prefix=home/theophile_gervet_gmail_com/hiveformer
-exp_src=02_16_regress_position_offset5
-exp_tgt=02_16_regress_position_offset5
+exp_src=02_17_train_with_offset2
+exp_tgt=02_17_train_with_offset2
 ckpt=best.pth
 
 # Get Tensorboard from source
 sshpass -p $MATRIX_PW rsync -R "$MATRIX:/$source_prefix/$exp_src/*/events.out*" .
 
 # Get checkpoints from source
-#sshpass -p $MATRIX_PW rsync -RL "$MATRIX:/$source_prefix/$exp_src/*/$ckpt" .
+sshpass -p $MATRIX_PW rsync -RL "$MATRIX:/$source_prefix/$exp_src/*/$ckpt" .
 
 # Send all to target
 scp -r $source_prefix/$exp_tgt $GCLOUD:/$target_prefix/
+
+
+# Uncleaned HiveFormer
+
+#source_prefix=home/tgervet/hiveformer/train_logs
+#source_prefix=home/tgervet/hiveformer/vln-robot/alhistory/xp
+#exp_src=uncleaned_code_02_15
+#exp_tgt=uncleaned_code_02_15
+#ckpt=checkpoints/epoch=0-step=90000.ckpt
+#
+## Get Tensorboard from source
+#sshpass -p $MATRIX_PW rsync -R "$MATRIX:/$source_prefix/$exp_src/lightning_logs/*/events.out*" .
+#
+## Get checkpoints from source
+#sshpass -p $MATRIX_PW rsync -RL "$MATRIX:/$source_prefix/$exp_src/lightning_logs/*/$ckpt" .
+#
+## Send all to target
+#scp -r $source_prefix/$exp_tgt $GCLOUD:/$target_prefix/
