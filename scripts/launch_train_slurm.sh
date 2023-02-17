ad8b288ddabcfe5123a86bf029d6f0a1612d4e46
Theophile Gervet <theophile.gervet@gmail.com>
2023-02-16 22:25:39 -0500

    Offset seems to work, let's try training

--- scripts/launch_train_slurm.sh
+++ scripts/launch_train_slurm.sh
@@ -14,93 +14,60 @@
 #     --train_iters 100_000
 #done
 
-main_dir=02_16_regress_position_offset5
-dataset=/home/tgervet/datasets/hiveformer/packaged/3
+main_dir=02_17_train_with_offset
+dataset=/home/tgervet/datasets/hiveformer/packaged/1
+valset=/home/tgervet/datasets/hiveformer/packaged/3
 image_size="256,256"
 num_workers=2
 task=put_money_in_safe
 num_ghost_points=2000
 batch_size=12
 
-#sbatch train_1gpu_32gb.sh \
-#     --tasks $task \
-#     --dataset $dataset \
-#     --image_size $image_size \
-#     --exp_log_dir $main_dir \
-#     --batch_size $batch_size \
-#     --num_workers $num_workers \
-#     --num_ghost_points $num_ghost_points \
-#     --regress_position_offset 0 \
-#     --run_log_dir OVERFIT-OLD-WAY-$task-NO-OFFSET
+sbatch train_1gpu_32gb.sh \
+     --tasks $task \
+     --dataset $dataset \
+     --valset $valset \
+     --image_size $image_size \
+     --exp_log_dir $main_dir \
+     --batch_size $batch_size \
+     --num_workers $num_workers \
+     --num_ghost_points $num_ghost_points \
+     --run_log_dir $task-NOLABELSMOOTHING \
+     --label_smoothing 0
 
-for position_offset_loss_coeff in 100 1000 10000; do
-  for points_supervised_for_offset in fine closest; do
-    sbatch train_1gpu_32gb.sh \
-         --tasks $task \
-         --dataset $dataset \
-         --image_size $image_size \
-         --exp_log_dir $main_dir \
-         --batch_size $batch_size \
-         --num_workers $num_workers \
-         --num_ghost_points $num_ghost_points \
-         --regress_position_offset 1 \
-         --position_offset_loss_coeff $position_offset_loss_coeff \
-         --points_supervised_for_offset $points_supervised_for_offset \
-         --run_log_dir OVERFIT-$task-$position_offset_loss_coeff-$points_supervised_for_offset
-  done
+sbatch train_1gpu_32gb.sh \
+     --tasks $task \
+     --dataset $dataset \
+     --valset $valset \
+     --image_size $image_size \
+     --exp_log_dir $main_dir \
+     --batch_size $batch_size \
+     --num_workers $num_workers \
+     --num_ghost_points $num_ghost_points \
+     --run_log_dir $task-LOSSATALLLAYERS \
+     --compute_loss_at_all_layers 1
+
+sbatch train_1gpu_32gb.sh \
+     --tasks $task \
+     --dataset $dataset \
+     --valset $valset \
+     --image_size $image_size \
+     --exp_log_dir $main_dir \
+     --batch_size $batch_size \
+     --num_workers $num_workers \
+     --num_ghost_points $num_ghost_points \
+     --run_log_dir $task-BIGGERLR \
+     --lr 5e-4
+
+for num_ghost_points in 500 1000 2000; do
+  sbatch train_1gpu_32gb.sh \
+       --tasks $task \
+       --dataset $dataset \
+       --valset $valset \
+       --image_size $image_size \
+       --exp_log_dir $main_dir \
+       --batch_size $batch_size \
+       --num_workers $num_workers \
+       --num_ghost_points $num_ghost_points \
+       --run_log_dir $task-$num_ghost_points-ghost_points
 done
-
-#main_dir=02_15_tune_ghost_points
-#dataset=/home/tgervet/datasets/hiveformer/packaged/3
-#image_size="256,256"
-#fine_sampling_cube_size=0.08
-#use_ground_truth_position_for_sampling_val=0
-#num_workers=2
-#task=put_money_in_safe
-#num_ghost_points=2000
-#batch_size=12
-#
-#sbatch train_1gpu_32gb.sh \
-#     --tasks $task \
-#     --dataset $dataset \
-#     --image_size $image_size \
-#     --exp_log_dir $main_dir \
-#     --batch_size $batch_size \
-#     --num_workers $num_workers \
-#     --num_ghost_points $num_ghost_points \
-#     --run_log_dir $task-NOLABELSMOOTHING \
-#     --label_smoothing 0
-#
-#sbatch train_1gpu_32gb.sh \
-#     --tasks $task \
-#     --dataset $dataset \
-#     --image_size $image_size \
-#     --exp_log_dir $main_dir \
-#     --batch_size $batch_size \
-#     --num_workers $num_workers \
-#     --num_ghost_points $num_ghost_points \
-#     --run_log_dir $task-LOSSATALLLAYERS \
-#     --compute_loss_at_all_layers 1
-#
-#sbatch train_1gpu_32gb.sh \
-#     --tasks $task \
-#     --dataset $dataset \
-#     --image_size $image_size \
-#     --exp_log_dir $main_dir \
-#     --batch_size $batch_size \
-#     --num_workers $num_workers \
-#     --num_ghost_points $num_ghost_points \
-#     --run_log_dir $task-BIGGERLR \
-#     --lr 5e-4
-#
-#for num_ghost_points in 500 1000 2000; do
-#  sbatch train_1gpu_32gb.sh \
-#       --tasks $task \
-#       --dataset $dataset \
-#       --image_size $image_size \
-#       --exp_log_dir $main_dir \
-#       --batch_size $batch_size \
-#       --num_workers $num_workers \
-#       --num_ghost_points $num_ghost_points \
-#       --run_log_dir $task-$num_ghost_points-ghost_points
-#done
