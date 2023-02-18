98f6df11a1a7e8d4a66b88487181fc4330b464e0
Theophile Gervet <theophile.gervet@gmail.com>
2023-02-18 14:19:15 -0500

    Eval

--- scripts/launch_eval_local.sh
+++ scripts/launch_eval_local.sh
@@ -7,13 +7,14 @@ tasks=(
 )
 data_dir=/home/theophile_gervet_gmail_com/datasets/hiveformer/raw/3
 image_size="256,256"
+num_episodes=10
 
 num_ckpts=${#ckpts[@]}
 for ((i=0; i<$num_ckpts; i++)); do
   python eval.py --tasks ${tasks[$i]} --checkpoint $exp/${ckpts[$i]}/best.pth \
-    --data_dir $data_dir --image_size $image_size --offline 1 \
+    --data_dir $data_dir --image_size $image_size --offline 1 --num_episodes $num_episodes \
     --exp_log_dir $exp --run_log_dir ${tasks[$i]}-OFFLINE &
   python eval.py --tasks ${tasks[$i]} --checkpoint $exp/${ckpts[$i]}/best.pth \
-    --data_dir $data_dir --image_size $image_size --offline 0 \
+    --data_dir $data_dir --image_size $image_size --offline 0 --num_episodes $num_episodes \
     --exp_log_dir $exp --run_log_dir ${tasks[$i]}-ONLINE &
 done
