53bd779711b55367d892f15f408719420e6f29a0
Theophile Gervet <theophile.gervet@gmail.com>
2023-02-21 07:37:16 -0500

    Rotation loss coeff is tricky to tune, train without rotation first

--- scripts/launch_train_slurm.sh
+++ scripts/launch_train_slurm.sh
@@ -3,16 +3,16 @@
 main_dir=02_20_compare_hiveformer_and_baseline
 task_file=tasks/10_autolambda_tasks.csv
 
-for task in $(cat $task_file | tr '\n' ' '); do
-  sbatch train_1gpu_12gb.sh \
-     --tasks $task \
-     --dataset /home/tgervet/datasets/hiveformer/packaged/0 \
-     --valset /home/tgervet/datasets/hiveformer/packaged/1 \
-     --image_size "128,128" \
-     --exp_log_dir $main_dir \
-     --model original \
-     --run_log_dir HIVEFORMER-$task
-done
+#for task in $(cat $task_file | tr '\n' ' '); do
+#  sbatch train_1gpu_12gb.sh \
+#     --tasks $task \
+#     --dataset /home/tgervet/datasets/hiveformer/packaged/0 \
+#     --valset /home/tgervet/datasets/hiveformer/packaged/1 \
+#     --image_size "128,128" \
+#     --exp_log_dir $main_dir \
+#     --model original \
+#     --run_log_dir HIVEFORMER-$task
+#done
 
 for task in $(cat $task_file | tr '\n' ' '); do
   sbatch train_1gpu_32gb.sh \
