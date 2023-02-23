99e087fc6bf64a72a19c9fcc749140e7da627e69
Theophile Gervet <theophile.gervet@gmail.com>
2023-02-23 16:38:11 -0500

    Fix GPU set

--- train_1gpu_32gb.sh
+++ train_1gpu_32gb.sh
@@ -6,9 +6,8 @@
 #SBATCH --output=slurm_logs/train-hiveformer-%j.out
 #SBATCH --error=slurm_logs/train-hiveformer-%j.err
 #SBATCH --time=3-00:00:00
-#SBATCH --gres=gpu:volta:1
-#SBATCH --mem=62gb
-#SBATCH --mem-per-gpu=32gb
-#SBATCH --exclude matrix-0-24
+#SBATCH --gres=gpu:1
+#SBATCH --mem=64gb
+#SBATCH --nodelist matrix-1-14,matrix-1-24,matrix-2-25,matrix-2-29,matrix-1-16
 
 python train.py "$@"
