80eb7d4e89507e5546e77900cfc633149fbbd51d
Theophile Gervet <theophile.gervet@gmail.com>
2023-02-23 16:58:05 -0500

    launch jobs

--- train_1gpu_32gb.sh
+++ train_1gpu_32gb.sh
@@ -7,6 +7,10 @@
 #SBATCH --time=3-00:00:00
 #SBATCH --gres=gpu:1
 #SBATCH --mem=64gb
-#SBATCH --nodelist matrix-1-14,matrix-1-24,matrix-2-25,matrix-2-29,matrix-1-16
+#SBATCH --nodelist matrix-1-14
+#SBATCH --nodelist matrix-1-24
+#SBATCH --nodelist matrix-2-25
+#SBATCH --nodelist matrix-2-29
+#SBATCH --nodelist matrix-1-16
 
 python train.py "$@"
