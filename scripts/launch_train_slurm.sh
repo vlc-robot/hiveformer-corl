ff1e087bcb1deeea1c8a19e7e79d644a6459a368
Theophile Gervet <theophile.gervet@gmail.com>
2023-02-23 07:47:28 -0500

    Launch jobs

--- scripts/launch_train_slurm.sh
+++ scripts/launch_train_slurm.sh
@@ -1,20 +1,21 @@
 #!/bin/sh
 
-main_dir=02_22_analogical_poc
-task=put_money_in_safe
+main_dir=02_23_analogical_poc
 
 for support_set in "self" "rest_of_batch"; do
-  sbatch train_1gpu_32gb.sh \
-     --tasks $task \
-     --dataset /home/tgervet/datasets/hiveformer/packaged/2 \
-     --valset /home/tgervet/datasets/hiveformer/packaged/3 \
-     --image_size "256,256" \
-     --exp_log_dir $main_dir \
-     --model baseline \
-     --batch_size 15 \
-     --num_workers 2 \
-     --position_prediction_only 1 \
-     --rotation_parametrization quat_from_top_ghost \
-     --support_set $support_set \
-     --run_log_dir $task-$support_set
+  for task in put_money_in_safe; do
+    sbatch train_1gpu_32gb.sh \
+       --tasks $task \
+       --dataset /home/tgervet/datasets/hiveformer/packaged/2 \
+       --valset /home/tgervet/datasets/hiveformer/packaged/3 \
+       --image_size "256,256" \
+       --exp_log_dir $main_dir \
+       --model analogical \
+       --batch_size 15 \
+       --num_workers 2 \
+       --position_prediction_only 1 \
+       --rotation_parametrization quat_from_top_ghost \
+       --support_set $support_set \
+       --run_log_dir $task-$support_set
+  done
 done
