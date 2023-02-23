32f51bdfc6462177d0c894e6007546abaa724cbd
Theophile Gervet <theophile.gervet@gmail.com>
2023-02-23 17:23:00 -0500

    Launch

--- train_1gpu_32gb_kate.sh
+++ train_1gpu_32gb_kate.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-#SBATCH --partition=russ_reserved,kate_reserved
+#SBATCH --partition=kate_reserved
 #SBATCH --job-name=analogical_manipulation
 #SBATCH --output=slurm_logs/train-hiveformer-%j.out
 #SBATCH --error=slurm_logs/train-hiveformer-%j.err
 #SBATCH --time=72:00:00
 #SBATCH --gres=gpu:1
 #SBATCH --mem=64gb
-#SBATCH --nodelist=matrix-1-14,matrix-1-24,matrix-2-25,matrix-2-29,matrix-1-16
+#SBATCH --nodelist=matrix-1-14,matrix-1-24,matrix-2-25,matrix-2-29
 
 python train.py "$@"
---
+++ train_1gpu_32gb_russ.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#SBATCH --partition=russ_reserved
+#SBATCH --job-name=analogical_manipulation
+#SBATCH --output=slurm_logs/train-hiveformer-%j.out
+#SBATCH --error=slurm_logs/train-hiveformer-%j.err
+#SBATCH --time=72:00:00
+#SBATCH --gres=gpu:1
+#SBATCH --mem=64gb
+#SBATCH --nodelist=matrix-1-16
+
+python train.py "$@"
