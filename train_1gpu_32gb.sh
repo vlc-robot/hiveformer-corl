c2c56a1c525b2f1ca04b59cc6f250baf1aef465a
Theophile Gervet <theophile.gervet@gmail.com>
2023-02-23 21:50:09 -0500

    Launch jobs

--- train_1gpu_32gb.sh
+++ train_1gpu_32gb.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-#SBATCH --partition=kate_reserved
+#SBATCH --partition=russ_reserved,kate_reserved
 #SBATCH --job-name=analogical_manipulation
 #SBATCH --output=slurm_logs/train-hiveformer-%j.out
 #SBATCH --error=slurm_logs/train-hiveformer-%j.err
 #SBATCH --time=72:00:00
 #SBATCH --gres=gpu:1
 #SBATCH --mem=64gb
-#SBATCH --nodelist=matrix-1-14,matrix-1-24,matrix-2-25,matrix-2-29
+#SBATCH --constraint="volta|A100"
+#SBATCH --exclude=matrix-0-24
 
 python train.py "$@"
