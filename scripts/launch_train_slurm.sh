783d800be29f0e467a2928974eb1c16e7266c8a6
Theophile Gervet <theophile.gervet@gmail.com>
2023-02-24 15:08:00 -0500

    Tune rot scale

--- scripts/launch_train_slurm.sh
+++ scripts/launch_train_slurm.sh
@@ -14,18 +14,20 @@ task_file=tasks/10_autolambda_tasks.csv
 #done
 
 main_dir=02_20_compare_hiveformer_and_baseline
-for task in $(cat $task_file | tr '\n' ' '); do
-  sbatch train_1gpu_32gb.sh \
-     --tasks $task \
-     --dataset /home/tgervet/datasets/hiveformer/packaged/2 \
-     --valset /home/tgervet/datasets/hiveformer/packaged/3 \
-     --image_size "256,256" \
-     --exp_log_dir $main_dir \
-     --model baseline \
-     --batch_size 15 \
-     --num_workers 2 \
-     --position_prediction_only 0 \
-     --run_log_dir BASELINE-WITH-100-ROTATION-SCALING-$task
+for task in pick_up_cup pick_and_lift put_money_in_safe; do
+  for rotation_loss_coeff in 1 10; do
+    sbatch train_1gpu_32gb.sh \
+       --tasks $task \
+       --dataset /home/tgervet/datasets/hiveformer/packaged/2 \
+       --valset /home/tgervet/datasets/hiveformer/packaged/3 \
+       --image_size "256,256" \
+       --exp_log_dir $main_dir \
+       --model baseline \
+       --batch_size 15 \
+       --num_workers 2 \
+       --position_prediction_only 0 \
+       --run_log_dir BASELINE-WITH-$rotation_loss_coeff-ROTATION-SCALING-$task
+  done
 done
 
 #main_dir=02_23_analogical_poc_for_all_tasks
