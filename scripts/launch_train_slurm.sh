a705056672d3fff937cbbb402ad2aa2e45c7d78c
Theophile Gervet <theophile.gervet@gmail.com>
2023-02-23 23:17:02 -0500

    launch jobs on 4 gpus

--- scripts/launch_train_slurm.sh
+++ scripts/launch_train_slurm.sh
@@ -13,22 +13,7 @@ task_file=tasks/10_autolambda_tasks.csv
 #     --run_log_dir HIVEFORMER-$task
 #done
 
-main_dir=02_20_compare_hiveformer_and_baseline
-for task in $(cat $task_file | tr '\n' ' '); do
-  sbatch train_1gpu_32gb.sh \
-     --tasks $task \
-     --dataset /home/tgervet/datasets/hiveformer/packaged/2 \
-     --valset /home/tgervet/datasets/hiveformer/packaged/3 \
-     --image_size "256,256" \
-     --exp_log_dir $main_dir \
-     --model baseline \
-     --batch_size 15 \
-     --num_workers 2 \
-     --position_prediction_only 0 \
-     --run_log_dir BASELINE-WITH-100-ROTATION-SCALING-$task
-done
-
-#main_dir=02_23_analogical_poc_for_all_tasks
+#main_dir=02_20_compare_hiveformer_and_baseline
 #for task in $(cat $task_file | tr '\n' ' '); do
 #  sbatch train_1gpu_32gb.sh \
 #     --tasks $task \
@@ -36,11 +21,27 @@ done
 #     --valset /home/tgervet/datasets/hiveformer/packaged/3 \
 #     --image_size "256,256" \
 #     --exp_log_dir $main_dir \
-#     --model analogical \
+#     --model baseline \
 #     --batch_size 15 \
 #     --num_workers 2 \
-#     --position_prediction_only 1 \
-#     --rotation_parametrization quat_from_top_ghost \
-#     --support_set rest_of_batch \
-#     --run_log_dir $task
+#     --position_prediction_only 0 \
+#     --run_log_dir BASELINE-WITH-100-ROTATION-SCALING-$task
 #done
+
+main_dir=02_24_analogical_poc_for_all_tasks
+for task in $(cat $task_file | tr '\n' ' '); do
+  sbatch train_4gpu_12gb.sh \
+     --tasks $task \
+     --dataset /home/tgervet/datasets/hiveformer/packaged/2 \
+     --valset /home/tgervet/datasets/hiveformer/packaged/3 \
+     --image_size "256,256" \
+     --exp_log_dir $main_dir \
+     --model analogical \
+     --batch_size 20 \
+     --num_workers 8 \
+     --position_prediction_only 1 \
+     --rotation_parametrization quat_from_top_ghost \
+     --support_set rest_of_batch \
+     --devices cuda:0 cuda:1 \
+     --run_log_dir $task
+done
--- train_2gpu_12gb.sh
+++ train_2gpu_12gb.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-#SBATCH --partition=russ_reserved
-#SBATCH --partition=kate_reserved
+#SBATCH --partition=russ_reserved,kate_reserved
 #SBATCH --exclude matrix-0-24,matrix-0-26,matrix-0-28,matrix-0-36,matrix-0-38,matrix-1-12,matrix-1-16,matrix-1-18,matrix-1-20,matrix-1-22
 #SBATCH --job-name=analogical_manipulation
 #SBATCH --output=slurm_logs/train-hiveformer-%j.out
 #SBATCH --error=slurm_logs/train-hiveformer-%j.err
 #SBATCH --time=3-00:00:00
-#SBATCH --gres=gpu:1
-#SBATCH --mem=50gb
+#SBATCH --gres=gpu:2
+#SBATCH --mem=100gb
 
 python train.py "$@"
