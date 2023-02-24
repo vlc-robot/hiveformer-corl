47d96d39865ee0b63438f9640857357b0536c26f
Theophile Gervet <theophile.gervet@gmail.com>
2023-02-23 21:55:12 -0500

    launch

--- train_1gpu_32gb.sh
+++ train_1gpu_32gb.sh
@@ -6,7 +6,7 @@
 #SBATCH --error=slurm_logs/train-hiveformer-%j.err
 #SBATCH --time=72:00:00
 #SBATCH --gres=gpu:1
-#SBATCH --mem=64gb
+#SBATCH --mem=50gb
 #SBATCH --constraint=volta|A100
 #SBATCH --exclude=matrix-0-24
 
