1988e2ab0b7a8d8bea7e2e7c10eb08718d7ec2b7
Theophile Gervet <theophile.gervet@gmail.com>
2023-02-24 18:59:51 -0500

    eval

--- scripts/launch_eval_local.sh
+++ scripts/launch_eval_local.sh
@@ -1,13 +1,17 @@
 exp=02_20_compare_hiveformer_and_baseline
 ckpts=(
-  BASELINE-WITH-100-ROTATION-SCALING-put_money_in_safe_version153634
-  BASELINE-WITH-100-ROTATION-SCALING-slide_block_to_target_version153848
-  BASELINE-WITH-100-ROTATION-SCALING-take_umbrella_out_of_umbrella_stand_version153852
+#  BASELINE-WITH-100-ROTATION-SCALING-put_money_in_safe_version153634
+#  BASELINE-WITH-100-ROTATION-SCALING-slide_block_to_target_version153848
+#  BASELINE-WITH-100-ROTATION-SCALING-take_umbrella_out_of_umbrella_stand_version153852
+  BASELINE-WITHOUT-ROTATION-take_money_out_safe_version153453
+  BASELINE-WITHOUT-ROTATION-put_knife_on_chopping_board_version153448
 )
 tasks=(
-  put_money_in_safe
-  slide_block_to_target
-  take_umbrella_out_of_umbrella_stand
+#  put_money_in_safe
+#  slide_block_to_target
+#  take_umbrella_out_of_umbrella_stand
+  take_money_out_safe
+  put_knife_on_chopping_board
 )
 data_dir=/home/theophile_gervet_gmail_com/datasets/hiveformer/raw/3
 image_size="256,256"
@@ -15,9 +19,12 @@ num_episodes=10
 
 num_ckpts=${#ckpts[@]}
 for ((i=0; i<$num_ckpts; i++)); do
+#  python eval.py --tasks ${tasks[$i]} --checkpoint $exp/${ckpts[$i]}/best.pth \
+#    --data_dir $data_dir --image_size $image_size --offline 0 --num_episodes $num_episodes \
+#    --exp_log_dir $exp --run_log_dir ${tasks[$i]}-ONLINE
   python eval.py --tasks ${tasks[$i]} --checkpoint $exp/${ckpts[$i]}/best.pth \
-    --data_dir $data_dir --image_size $image_size --offline 0 --num_episodes $num_episodes \
-    --exp_log_dir $exp --run_log_dir ${tasks[$i]}-ONLINE
+    --data_dir $data_dir --image_size $image_size --offline 1 --num_episodes $num_episodes \
+    --exp_log_dir $exp --run_log_dir ${tasks[$i]}-OFFLINE
 done
 
 
