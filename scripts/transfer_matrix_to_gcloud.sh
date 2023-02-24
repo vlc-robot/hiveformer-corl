3460187e0c2c1c026f031d104a1ec4c50c608c23
Theophile Gervet <theophile.gervet@gmail.com>
2023-02-23 21:25:15 -0500

    Launch

--- scripts/transfer_matrix_to_gcloud.sh
+++ scripts/transfer_matrix_to_gcloud.sh
@@ -2,10 +2,10 @@
 
 source_prefix=home/tgervet/hiveformer/train_logs
 target_prefix=home/theophile_gervet_gmail_com/hiveformer
-exp_src=02_23_analogical_poc
-exp_tgt=02_23_analogical_poc
-#exp_src=02_17_overfit_rotation2
-#exp_tgt=02_17_overfit_rotation2
+#exp_src=02_23_analogical_poc
+#exp_tgt=02_23_analogical_poc
+exp_src=02_20_compare_hiveformer_and_baseline
+exp_tgt=02_20_compare_hiveformer_and_baseline
 ckpt=best.pth
 
 # Get Tensorboard from source
