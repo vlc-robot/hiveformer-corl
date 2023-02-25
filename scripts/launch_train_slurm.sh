4264b7bfa0d2dbcdc4bd5c5e9bfefb8ad246e83d
Theophile Gervet <theophile.gervet@gmail.com>
2023-02-25 11:07:14 -0500

    fine grained vis features

--- scripts/launch_train_slurm.sh
+++ scripts/launch_train_slurm.sh
@@ -7,9 +7,9 @@ main_dir=02_25_increase_visual_resolution
 dataset=/home/tgervet/datasets/hiveformer/packaged/2
 valset=/home/tgervet/datasets/hiveformer/packaged/3
 image_size="256,256"
-for task in take_money_out_safe put_knife_on_chopping_board put_money_in_safe; do
-  sbatch train_4gpu_12gb.sh \
-     --devices cuda:0 cuda:1 cuda:2 cuda:3 \
+for task in take_money_out_safe put_knife_on_chopping_board; do
+for task in put_knife_on_chopping_board; do
+  sbatch train_1gpu_32gb.sh \
      --tasks $task \
      --dataset $dataset \
      --valset $valset \
@@ -25,9 +25,8 @@ done
 dataset=/home/tgervet/datasets/hiveformer/packaged/0
 valset=/home/tgervet/datasets/hiveformer/packaged/1
 image_size="128,128"
-for task in take_money_out_safe put_knife_on_chopping_board put_money_in_safe; do
-  sbatch train_4gpu_12gb.sh \
-     --devices cuda:0 cuda:1 cuda:2 cuda:3 \
+for task in take_money_out_safe put_knife_on_chopping_board; do
+  sbatch train_1gpu_32gb.sh \
      --tasks $task \
      --dataset $dataset \
      --valset $valset \
