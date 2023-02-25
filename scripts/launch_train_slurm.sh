6fd328c5e170f6a464f0e0b5fb1fe982527d526d
Theophile Gervet <theophile.gervet@gmail.com>
2023-02-25 11:33:03 -0500

    fine grained vis features

--- scripts/launch_train_slurm.sh
+++ scripts/launch_train_slurm.sh
@@ -2,29 +2,33 @@
 
 task_file=tasks/10_autolambda_tasks.csv
 
-main_dir=02_25_increase_visual_resolution
+main_dir=02_25_increase_visual_resolution2
 
 dataset=/home/tgervet/datasets/hiveformer/packaged/2
 valset=/home/tgervet/datasets/hiveformer/packaged/3
 image_size="256,256"
 for task in take_money_out_safe put_knife_on_chopping_board; do
-  sbatch train_1gpu_32gb.sh \
-     --tasks $task \
-     --dataset $dataset \
-     --valset $valset \
-     --image_size $image_size \
-     --exp_log_dir $main_dir \
-     --model baseline \
-     --batch_size 16 \
-     --num_workers 4 \
-     --position_prediction_only 1 \
-     --run_log_dir $task-$image_size
+  for position_offset_loss_coeff in 10000 100; do
+    sbatch train_1gpu_32gb.sh \
+       --tasks $task \
+       --dataset $dataset \
+       --valset $valset \
+       --image_size $image_size \
+       --exp_log_dir $main_dir \
+       --model baseline \
+       --batch_size 16 \
+       --num_workers 4 \
+       --position_prediction_only 1 \
+       --position_offset_loss_coeff $position_offset_loss_coeff \
+       --run_log_dir $task-$image_size-$position_offset_loss_coeff
+  done
 done
 
 dataset=/home/tgervet/datasets/hiveformer/packaged/0
 valset=/home/tgervet/datasets/hiveformer/packaged/1
 image_size="128,128"
 for task in take_money_out_safe put_knife_on_chopping_board; do
+  for position_offset_loss_coeff in 10000 100; do
   sbatch train_1gpu_32gb.sh \
      --tasks $task \
      --dataset $dataset \
