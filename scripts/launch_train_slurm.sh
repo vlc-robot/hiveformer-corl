c96a4ffa3db503179bfe9ee0d0676a1d29a8ff0f
Theophile Gervet <theophile.gervet@gmail.com>
2023-02-28 15:52:26 -0500

    Check for regressions with large workspace

--- scripts/launch_train_slurm.sh
+++ scripts/launch_train_slurm.sh
@@ -5,40 +5,41 @@ dataset=/home/tgervet/datasets/hiveformer/packaged/2
 valset=/home/tgervet/datasets/hiveformer/packaged/3
 
 # Big model with big batch size on big GPUs
-task_file=tasks/7_interesting_tasks.csv
-for use_instruction in 1; do
-  sbatch train_4gpu_32gb.sh \
-     --devices cuda:0 cuda:1 cuda:2 cuda:3 \
-     --tasks $(cat $task_file | tr '\n' ' ') \
-     --dataset $dataset \
-     --valset $valset \
-     --exp_log_dir $main_dir \
-     --use_instruction $use_instruction \
-     --num_workers 16 \
-     --batch_size 32 \
-     --embedding_dim 120 \
-     --lr 1e-4 \
-     --run_log_dir LARGE-MULTITASK-use-instruction-$use_instruction
-done
+#task_file=tasks/7_interesting_tasks.csv
+#for use_instruction in 1; do
+#  sbatch train_4gpu_32gb.sh \
+#     --devices cuda:0 cuda:1 cuda:2 cuda:3 \
+#     --tasks $(cat $task_file | tr '\n' ' ') \
+#     --dataset $dataset \
+#     --valset $valset \
+#     --exp_log_dir $main_dir \
+#     --use_instruction $use_instruction \
+#     --num_workers 16 \
+#     --batch_size 32 \
+#     --embedding_dim 120 \
+#     --lr 1e-4 \
+#     --run_log_dir LARGE-MULTITASK-use-instruction-$use_instruction
+#done
 
 # Small model with and without and without instructions on small GPUs
-task_file=tasks/7_interesting_tasks.csv
-for use_instruction in 0 1; do
-  sbatch train_4gpu_12gb.sh \
-     --devices cuda:0 cuda:1 cuda:2 cuda:3 \
-     --tasks $(cat $task_file | tr '\n' ' ') \
-     --dataset $dataset \
-     --valset $valset \
-     --exp_log_dir $main_dir \
-     --use_instruction $use_instruction \
-     --num_workers 12 \
-     --batch_size 16 \
-     --run_log_dir SMALL-MULTITASK-use-instruction-$use_instruction
-done
+#task_file=tasks/7_interesting_tasks.csv
+#for use_instruction in 0 1; do
+#  sbatch train_4gpu_12gb.sh \
+#     --devices cuda:0 cuda:1 cuda:2 cuda:3 \
+#     --tasks $(cat $task_file | tr '\n' ' ') \
+#     --dataset $dataset \
+#     --valset $valset \
+#     --exp_log_dir $main_dir \
+#     --use_instruction $use_instruction \
+#     --num_workers 12 \
+#     --batch_size 16 \
+#     --run_log_dir SMALL-MULTITASK-use-instruction-$use_instruction
+#done
 
 # Check that there were no regressions with using a large workspace
+main_dir=02_28_check_for_regressions
 for task in put_knife_on_chopping_board take_money_out_safe; do
-  for num_ghost_points in 1000 2000; do
+  for num_ghost_points in 1000; do
     sbatch train_1gpu_32gb.sh \
        --tasks $task \
        --dataset $dataset \
