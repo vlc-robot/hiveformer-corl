cb116ba88d5c7426db5a5021d101096a5d690cb1
Theophile Gervet <theophile.gervet@gmail.com>
2023-02-28 08:09:03 -0500

    Add -u

--- scripts/transfer_matrix_to_gcloud.sh
+++ scripts/transfer_matrix_to_gcloud.sh
@@ -1,19 +1,31 @@
-# Ours
+# To Google Cloud
+
+#source_prefix=home/tgervet/hiveformer/train_logs
+#target_prefix=home/theophile_gervet_gmail_com/hiveformer
+#exp_src=02_27_multi_task
+#exp_tgt=02_27_multi_task
+#ckpt=best.pth
+#
+## Get Tensorboard from source
+#sshpass -p $MATRIX_PW rsync -R "$MATRIX:/$source_prefix/$exp_src/*/events.out*" .
+#
+## Get checkpoints from source
+##sshpass -p $MATRIX_PW rsync -RL "$MATRIX:/$source_prefix/$exp_src/*/$ckpt" .
+#
+## Send all to target
+#scp -r $source_prefix/$exp_tgt $GCLOUD:/$target_prefix/
+
+
+# To Xian
 
 source_prefix=home/tgervet/hiveformer/train_logs
-target_prefix=home/theophile_gervet_gmail_com/hiveformer
-exp_src=02_25_increase_visual_resolution2
-exp_tgt=02_25_increase_visual_resolution2
+target_prefix=/home/zhouxian/git/datasets/hiveformer
+exp_src=02_27_multi_task
+exp_tgt=02_27_multi_task
 ckpt=best.pth
 
-# Get Tensorboard from source
-sshpass -p $MATRIX_PW rsync -R "$MATRIX:/$source_prefix/$exp_src/*/events.out*" .
-
-# Get checkpoints from source
-#sshpass -p $MATRIX_PW rsync -RL "$MATRIX:/$source_prefix/$exp_src/*/$ckpt" .
-
-# Send all to target
-scp -r $source_prefix/$exp_tgt $GCLOUD:/$target_prefix/
+# Tensorboard
+sshpass -p $LAB_PW rsync -R $source_prefix/$exp_src/*/events.out*" $LAB:$target_prefix
 
 
 # Uncleaned HiveFormer
