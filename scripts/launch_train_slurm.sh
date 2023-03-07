df5c023bca7e8ecea9ec2fb05c8dddd665addf16
Theophile Gervet <theophile.gervet@gmail.com>
2023-03-07 13:51:43 -0500

    HiveFormer 10 demos

--- scripts/launch_train_slurm.sh
+++ scripts/launch_train_slurm.sh
@@ -64,12 +64,12 @@ main_dir=exp
 #  --run_log_dir ANALOGY-MULTI-TASK
 # --------------------------------------------------------------------------------------------
 
-main_dir=03_06_task_specific_biases2
-embedding_dim=120
-num_workers=12
-train_iters=500_000
-task_file=tasks/7_interesting_tasks.csv
-
+#main_dir=03_06_task_specific_biases2
+#embedding_dim=120
+#num_workers=12
+#train_iters=500_000
+#task_file=tasks/7_interesting_tasks.csv
+#
 #batch_size=8
 #for task_specific_biases in 0 1; do
 #  sbatch train_4gpu_12gb.sh \
@@ -87,19 +87,33 @@ task_file=tasks/7_interesting_tasks.csv
 #    --embedding_dim $embedding_dim \
 #    --run_log_dir ANALOGY-task_specific_biases-$task_specific_biases
 #done
+#
+#batch_size=16
+#for task_specific_biases in 0 1; do
+#  sbatch train_4gpu_12gb.sh \
+#    --devices cuda:0 cuda:1 cuda:2 cuda:3 \
+#    --model baseline \
+#    --tasks $(cat $task_file | tr '\n' ' ') \
+#    --dataset $dataset \
+#    --valset $valset \
+#    --exp_log_dir $main_dir \
+#    --num_workers $num_workers \
+#    --batch_size $batch_size \
+#    --train_iters $train_iters \
+#    --embedding_dim $embedding_dim \
+#    --run_log_dir BASELINE-task_specific_biases-$task_specific_biases
+#done
 
-batch_size=16
-for task_specific_biases in 1; do
-  sbatch train_4gpu_12gb.sh \
-    --devices cuda:0 cuda:1 cuda:2 cuda:3 \
-    --model baseline \
-    --tasks $(cat $task_file | tr '\n' ' ') \
-    --dataset $dataset \
-    --valset $valset \
-    --exp_log_dir $main_dir \
-    --num_workers $num_workers \
-    --batch_size $batch_size \
-    --train_iters $train_iters \
-    --embedding_dim $embedding_dim \
-    --run_log_dir BASELINE-task_specific_biases-$task_specific_biases
+main_dir=03_07_hiveformer_10_demos
+task_file=tasks/7_interesting_tasks.csv
+for task in $(cat $task_file | tr '\n' ' '); do
+  sbatch train_1gpu_12gb.sh \
+     --tasks $task \
+     --dataset /home/tgervet/datasets/hiveformer/packaged/0 \
+     --valset /home/tgervet/datasets/hiveformer/packaged/1 \
+     --image_size "128,128" \
+     --exp_log_dir $main_dir \
+     --model original \
+     --max_episodes_per_taskvar 10 \
+     --run_log_dir $task
 done
