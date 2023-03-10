93d432e721373b3718c28b3d479acdb480134dce
Theophile Gervet <theophile.gervet@gmail.com>
2023-03-10 09:23:45 -0500

    Fix data gen

--- data_preprocessing/validate_data_generation.py
+++ data_preprocessing/validate_data_generation.py
@@ -10,5 +10,8 @@ TASK_FILE = "tasks/74_hiveformer_tasks.csv"
 
 
 if __name__ == "__main__":
-    raw_files = glob.glob(f"{DATA_DIR}/{VAL_DIR}/*")
-    print(raw_files)
+    raw_val_dirs = glob.glob(f"{DATA_DIR}/{VAL_DIR}/*")
+    for raw_val_dir in raw_val_dirs:
+        task = raw_val_dir.split("/")[-1]
+        variation_dirs = glob.glob(f"{raw_val_dir}/*")
+        print(f"{task}: {len(variation_dirs)} variations")
