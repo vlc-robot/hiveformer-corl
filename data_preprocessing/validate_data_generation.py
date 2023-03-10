1ea5308b10a57960faa04317817b98d6a53d26a6
Theophile Gervet <theophile.gervet@gmail.com>
2023-03-10 09:20:05 -0500

    Fix data gen

--- data_preprocessing/validate_data_generation.py
+++ data_preprocessing/validate_data_generation.py
@@ -4,8 +4,8 @@ import glob
 ROOT = "/home/zhouxian/git"
 DATA_DIR = f"{ROOT}/datasets/raw"
 OUTPUT_DIR = f"{ROOT}/datasets/packaged"
-TRAIN_DIR = "74_hiveformer_tasks_train"
-VAL_DIR = "74_hiveformer_tasks_val"
+TRAIN_DIR = "18_peract_tasks_train"
+VAL_DIR = "18_peract_tasks_val"
 TASK_FILE = "tasks/74_hiveformer_tasks.csv"
 
 
