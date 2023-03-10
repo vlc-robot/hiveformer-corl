fd8b22caf42dbb2bf649ab7c59b9532aa0a43ccb
Theophile Gervet <theophile.gervet@gmail.com>
2023-03-10 09:17:46 -0500

    Fix data gen

---
+++ data_preprocessing/validate_data_generation.py
@@ -0,0 +1,14 @@
+import glob
+
+
+ROOT = "/home/zhouxian/git"
+DATA_DIR = f"{ROOT}/datasets/raw"
+OUTPUT_DIR = f"{ROOT}/datasets/packaged"
+TRAIN_DIR = "74_hiveformer_tasks_train"
+VAL_DIR = "74_hiveformer_tasks_val"
+TASK_FILE = "tasks/74_hiveformer_tasks.csv"
+
+
+if __name__ == "__main__":
+    raw_files = glob.glob(f"{DATA_DIR}/{TRAIN_DIR}/*")
+    print(raw_files)
