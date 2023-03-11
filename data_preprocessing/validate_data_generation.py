d3dc0cd008c0233a7a128182c5f1511c432ce0b1
Theophile Gervet <theophile.gervet@gmail.com>
2023-03-11 17:09:05 -0500

    Validate data gen

--- data_preprocessing/validate_data_generation.py
+++ data_preprocessing/validate_data_generation.py
@@ -1,58 +1,93 @@
 import glob
+import tap
+from typing import Tuple
+
+from utils.utils_with_rlbench import RLBenchEnv
+from utils.utils_without_rlbench import load_episodes
 
 
-# RAW_DIR = f"/projects/katefgroup/analogical_manipulation/rlbench/raw"
-# PACKAGED_DIR = f"/projects/katefgroup/analogical_manipulation/rlbench/packaged"
-RAW_DIR = f"/home/zhouxian/git/datasets/raw"
-PACKAGED_DIR = f"/home/zhouxian/git/datasets/packaged"
+class Arguments(tap.Tap):
+    cameras: Tuple[str, ...] = ("left_shoulder", "right_shoulder", "wrist")
+    image_size: str = "256,256"
+    headless: bool = False
+    max_tries: int = 10
 
-# TRAIN_DIR = "18_peract_tasks_train"
-# VAL_DIR = "18_peract_tasks_val"
-# TRAIN_DIR = "74_hiveformer_tasks_train"
-# VAL_DIR = "74_hiveformer_tasks_val"
-TRAIN_DIR = "peract_problematic_tasks_train"
-VAL_DIR = "peract_problematic_tasks_val"
+    # raw_dir: str = "/projects/katefgroup/analogical_manipulation/rlbench/raw"
+    # packaged_dir: str = "/projects/katefgroup/analogical_manipulation/rlbench/packaged"
+    raw_dir: str = "/home/zhouxian/git/datasets/raw"
+    packaged_dir: str = "/home/zhouxian/git/datasets/packaged"
 
-VALIDATE_NUM_EPISODES = False
-VALIDATE_SUCCESSFUL_DEMOS = True
+    # train_dir: str = "18_peract_tasks_train"
+    # val_dir: str = "18_peract_tasks_val"
+    # train_dir: str = "74_hiveformer_tasks_train"
+    # val_dir: str = "74_hiveformer_tasks_val"
+    train_dir: str = "peract_problematic_tasks_train"
+    val_dir: str = "peract_problematic_tasks_val"
+
+    validate_num_episodes: int = 0
+    validate_successful_demos: int = 1
 
 
 if __name__ == "__main__":
+    args = Arguments().parse_args()
+
     # Validate that we have generated the expected number of episodes for each task
-    if VALIDATE_NUM_EPISODES:
-        for split in [TRAIN_DIR, VAL_DIR]:
+    if args.validate_num_episodes:
+        for split in [args.train_dir, args.val_dir]:
             print()
             print()
             print()
             print("Split: ", split)
             print()
 
-            raw_dirs = glob.glob(f"{RAW_DIR}/{split}/*")
+            raw_dirs = glob.glob(f"{args.raw_dir}/{split}/*")
 
-            for raw_val_dir in raw_dirs:
-                task = raw_val_dir.split("/")[-1]
-                raw_variation_dirs = glob.glob(f"{raw_val_dir}/*")
-                packaged_variation_dirs = glob.glob(f"{PACKAGED_DIR}/{split}/{task}*")
+            for raw_dir in raw_dirs:
+                task_str = raw_dir.split("/")[-1]
+                raw_variation_dirs = glob.glob(f"{raw_dir}/*")
+                packaged_variation_dirs = glob.glob(f"{args.packaged_dir}/{split}/{task_str}*")
                 raw_eps_per_variation = [len(glob.glob(f"{d}/episodes/*")) for d in raw_variation_dirs]
                 packaged_eps_per_variation = [len(glob.glob(f"{d}/*")) for d in packaged_variation_dirs]
                 print("=========================================")
-                print(task)
+                print(task_str)
                 print(f"Variations: {len(raw_variation_dirs)} raw, {len(packaged_variation_dirs)} packaged")
                 print(f"Episodes per variation: {raw_eps_per_variation} raw, {packaged_eps_per_variation} packaged")
                 print(f"Total episodes: {sum(raw_eps_per_variation)} raw, {sum(packaged_eps_per_variation)} packaged")
 
     # Validate that the generated demos are successful
-    if VALIDATE_SUCCESSFUL_DEMOS:
-        for split in [TRAIN_DIR, VAL_DIR]:
+    if args.validate_successful_demos:
+        max_eps_dict = load_episodes()["max_episode_length"]
+
+        for split in [args.train_dir, args.val_dir]:
             print()
             print()
             print()
             print("Split: ", split)
             print()
 
-            raw_dirs = glob.glob(f"{RAW_DIR}/{split}/*")
+            env = RLBenchEnv(
+                data_path=f"{args.raw_dir}/{split}",
+                image_size=[int(x) for x in args.image_size.split(",")],
+                apply_rgb=True,
+                apply_pc=True,
+                headless=args.headless,
+                apply_cameras=args.cameras,
+            )
 
-            for raw_val_dir in raw_dirs:
-                task = raw_val_dir.split("/")[-1]
-                raw_variation_dirs = glob.glob(f"{raw_val_dir}/*")
-                print(raw_variation_dirs)
+            task_dirs = glob.glob(f"{args.raw_dir}/{split}/*")
+
+            for task_dir in task_dirs:
+                task_str = task_dir.split("/")[-1]
+                var_dirs = glob.glob(f"{task_dir}/*")
+
+                for var_dir in var_dirs:
+                    variation = int(var_dir.split("variation")[-1])
+
+                    ep_dirs = glob.glob(f"{var_dir}/episodes/*")
+
+                    env.verify_demos(
+                        task_str=task_str,
+                        variation=variation,
+                        num_demos=len(ep_dirs),
+                        max_tries=args.max_tries
+                    )
