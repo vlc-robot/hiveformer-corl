063d0e29230a8aae45f85025f573a3e4a0affa99
Theophile Gervet <theophile.gervet@gmail.com>
2023-03-15 21:14:41 -0400

    Debug why close_jar doesn't work

--- utils/utils_with_rlbench.py
+++ utils/utils_with_rlbench.py
@@ -744,14 +744,14 @@ class RLBenchEnv:
 
 
 # Identify way-point in each RLBench Demo
-def _is_stopped(demo, i, obs, stopped_buffer):
+def _is_stopped(demo, i, obs, stopped_buffer, delta):
     next_is_not_final = i == (len(demo) - 2)
     gripper_state_no_change = i < (len(demo) - 2) and (
         obs.gripper_open == demo[i + 1].gripper_open
         and obs.gripper_open == demo[i - 1].gripper_open
         and demo[i - 2].gripper_open == demo[i - 1].gripper_open
     )
-    small_delta = np.allclose(obs.joint_velocities, 0, atol=0.1)
+    small_delta = np.allclose(obs.joint_velocities, 0, atol=delta)
     stopped = (
         stopped_buffer <= 0
         and small_delta
@@ -761,25 +761,26 @@ def _is_stopped(demo, i, obs, stopped_buffer):
     return stopped
 
 
-def keypoint_discovery(demo: Demo) -> List[int]:
+def keypoint_discovery(demo: Demo, stopping_delta=0.1) -> List[int]:
     episode_keypoints = []
     prev_gripper_open = demo[0].gripper_open
     stopped_buffer = 0
+
     for i, obs in enumerate(demo):
-        stopped = _is_stopped(demo, i, obs, stopped_buffer)
+        stopped = _is_stopped(demo, i, obs, stopped_buffer, stopping_delta)
         stopped_buffer = 4 if stopped else stopped_buffer - 1
         # If change in gripper, or end of episode.
         last = i == (len(demo) - 1)
         if i != 0 and (obs.gripper_open != prev_gripper_open or last or stopped):
             episode_keypoints.append(i)
         prev_gripper_open = obs.gripper_open
+
     if (
         len(episode_keypoints) > 1
         and (episode_keypoints[-1] - 1) == episode_keypoints[-2]
     ):
         episode_keypoints.pop(-2)
 
-    # HACK for tower3 task
     return episode_keypoints
 
 
