ef12f6f1a6c650788925a962f60d5c0d69e873cd
Theophile Gervet <theophile.gervet@gmail.com>
2023-03-28 10:24:06 -0400

    Launch eval

--- scripts/launch_eval_local.sh
+++ scripts/launch_eval_local.sh
@@ -1,14 +1,5 @@
 exp=03_24_hiveformer_setting
 ckpts=(
-  open_microwave-HIVEFORMER_version161074
-  open_grill-HIVEFORMER_version161073
-  open_drawer-HIVEFORMER_version161071
-  open_fridge-HIVEFORMER_version161072
-  lift_numbered_block-HIVEFORMER_version161069
-  open_box-HIVEFORMER_version161070
-  close_door-HIVEFORMER_version161046
-  unplug_charger-HIVEFORMER_version161045
-  lamp_on-HIVEFORMER_version161047
   turn_tap-HIVEFORMER_version160617
   push_button-HIVEFORMER_version160614
   slide_block_to_target-HIVEFORMER_version160615
@@ -19,17 +10,28 @@ ckpts=(
   close_fridge-HIVEFORMER_version160610
   close_microwave-HIVEFORMER_version160611
   reach_target-HIVEFORMER_version160608
+  close_grill-HIVEFORMER_version161176
+  beat_the_buzz-HIVEFORMER_version161174
+  change_clock-HIVEFORMER_version161175
+  basketball_in_hoop-HIVEFORMER_version161173
+  pick_up_cup-HIVEFORMER_version161167
+  play_jenga-HIVEFORMER_version161168
+  take_lid_off_saucepan-HIVEFORMER_version161169
+  toilet_seat_up-HIVEFORMER_version161171
+  take_umbrella_out_of_umbrella_stand-HIVEFORMER_version161170
+  turn_oven_on-HIVEFORMER_version161172
+  open_wine_bottle-HIVEFORMER_version161075
+  open_microwave-HIVEFORMER_version161074
+  open_grill-HIVEFORMER_version161073
+  open_drawer-HIVEFORMER_version161071
+  open_fridge-HIVEFORMER_version161072
+  lift_numbered_block-HIVEFORMER_version161069
+  open_box-HIVEFORMER_version161070
+  close_door-HIVEFORMER_version161046
+  unplug_charger-HIVEFORMER_version161045
+  lamp_on-HIVEFORMER_version161047
 )
 tasks=(
-  open_microwave
-  open_grill
-  open_drawer
-  open_fridge
-  lift_numbered_block
-  open_box
-  close_door
-  unplug_charger
-  lamp_on
   turn_tap
   push_button
   slide_block_to_target
@@ -40,15 +42,35 @@ tasks=(
   close_fridge
   close_microwave
   reach_target
+  close_grill
+  beat_the_buzz
+  change_clock
+  basketball_in_hoop
+  pick_up_cup
+  play_jenga
+  take_lid_off_saucepan
+  toilet_seat_up
+  take_umbrella_out_of_umbrella_stand
+  turn_oven_on
+  open_wine_bottle
+  open_microwave
+  open_grill
+  open_drawer
+  open_fridge
+  lift_numbered_block
+  open_box
+  close_door
+  unplug_charger
+  lamp_on
 )
 data_dir=/home/zhouxian/git/datasets/raw/74_hiveformer_tasks_val
-num_episodes=100
+num_episodes=2
 gripper_loc_bounds_file=tasks/74_hiveformer_tasks_location_bounds.json
 
 num_ckpts=${#ckpts[@]}
 for ((i=0; i<$num_ckpts; i++)); do
   python eval.py --tasks ${tasks[$i]} --checkpoint $exp/${ckpts[$i]}/best.pth \
-    --data_dir $data_dir --offline 0 --num_episodes $num_episodes --headless 1 --output_file eval/${tasks[$i]}.json  \
+    --data_dir $data_dir --offline 0 --num_episodes $num_episodes --headless 0 --output_file eval/${tasks[$i]}.json  \
     --exp_log_dir $exp --run_log_dir ${tasks[$i]}-ONLINE --record_videos 0 --use_instruction $use_instruction \
     --gripper_loc_bounds_file $gripper_loc_bounds_file
     # --variations {0..60}
