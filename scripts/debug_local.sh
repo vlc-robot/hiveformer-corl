0214b391cdeec45410cf0abdbdca7a596744775c
Theophile Gervet <theophile.gervet@gmail.com>
2023-04-26 18:12:59 -0400

    debug

--- scripts/debug_local.sh
+++ scripts/debug_local.sh
@@ -14,14 +14,12 @@ use_instruction=0
 num_ghost_points=10000
 headless=0
 offline=0
-record_videos=0
-max_steps=10
 
 num_ckpts=${#ckpts[@]}
 for ((i=0; i<$num_ckpts; i++)); do
   python eval.py --tasks ${tasks[$i]} --checkpoint $exp/${ckpts[$i]}/best.pth \
     --data_dir $data_dir --offline $offline --num_episodes $num_episodes --headless $headless --output_file eval/${tasks[$i]}.json  \
-    --exp_log_dir $exp --run_log_dir ${tasks[$i]}-ONLINE --record_videos $record_videos --use_instruction $use_instruction --max_steps $max_steps \
+    --exp_log_dir $exp --run_log_dir ${tasks[$i]}-ONLINE --record_videos 0 --use_instruction $use_instruction \
     --gripper_loc_bounds_file $gripper_loc_bounds_file --num_ghost_points $num_ghost_points --num_ghost_points_val $num_ghost_points \
     --output_file /home/sirdome/katefgroup/hiveformer/eval_new.json
 #    --variations {0..60}
